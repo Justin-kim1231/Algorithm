75d8a6f9f3a86aca3df6d5b10ec1bcd1f2a17154
Justin-kim1231 <112284029+Justin-kim1231@users.noreply.github.com>
2022-08-29 22:52:23 -0500

    Update 로또의 최고 순위와 최저순위.java

--- 로또의 최고 순위와 최저순위.java
+++ 로또의 최고 순위와 최저순위.java
@@ -2,7 +2,9 @@ class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
         int zero = 0;
         int matched = 0;
-        
+        /**
+         *int l은 로또안에 있는 숫자이고 for (int l : lottos)는 로또 안에 있는 숫자들을 나타낸다.
+         */
         for (int l : lottos) {
             if (l == 0) zero++;
             else {
