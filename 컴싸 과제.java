654872e0e35ae790748bbfffab3df6fd6fb8e3c7
Justin-kim1231 <112284029+Justin-kim1231@users.noreply.github.com>
2022-09-02 22:01:43 -0500

    Update 컴싸 과제.java
    MAX  method 추가

--- 컴싸 과제.java
+++ 컴싸 과제.java
@@ -10,3 +10,19 @@
       }
       return min;
    }
+
+public static int max(int[] a) throws IllegalArgumentException { 
+      if (a == null || a.length == 0) {
+         throw new IllegalArgumentException("Array is null or it has zero length");
+      }
+      int max = a[0]; 
+      for(int i = 0; i < a.length; i++) {
+      
+         if (a[i] > max)
+            max = a[i];
+      }
+   
+   
+      return max;
+   }
+
